bf67a1823af4f8e8c9957d065d3f79eae563892b
beratkbu <sjsberat@gmail.com>
2026-06-27 14:43:27 +0300

    veritabani duzeltmesi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot(): void
     {
-        //
+        // Render gibi canlı ortamlarda veritabanını otomatik kur
+        if ($this->app->environment('production')) {
+            Artisan::call('migrate --force');
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function register(): void
     {
         //
     }
